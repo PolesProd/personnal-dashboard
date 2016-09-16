46d5490c0a5dbfaf35de7b92619d4494f233a0b2
Gootime <gotimeingame@gmail.com>
2016-09-16 16:24:22 +0200

    getContext

--- index.php
+++ index.php
@@ -74,57 +74,44 @@ include('lib/rssclass.php');
                 <div class="WidgetGrand">
                     <canvas id="myChart" width="800" height="650"></canvas>
                     <script type="text/javascript">
-                        var N = 10;
-                        // Array filled with N values at '0'
-                        var zero_array = [];
-
-                        for (i = 0; i < N; i++)
-                            zero_array.push(0);
-
-                        // The data of the chart, describe the differents sets of data you want with points, colors...
-                        var data = {
-                                labels: zero_array,
-                                datasets:  [
-                                    {
-                                        label: "DataSet #1", // Name of the line
-                                        data: zero_array, // data to represent
-                                        // The following makes the line way less ugly
-                                        fillColor: "rgba(151,187,205,0.2)",
-                                        strokeColor: "rgba(151,187,205,1)",
-                                        pointColor: "rgba(151,187,205,1)",
-                                        pointStrokeColor: "#fff",
-                                        pointHighlightFill: "#fff",
-                                        pointHighlightStroke: "rgba(151,187,205,1)"
-                                    }
-                                ]
-                        };
-
-                        // We wait for everything to be loaded
-                        window.onload = function main() {
-
-                            // Get the context of the canvas
-                            var ctx = document.getElementById("myChart");
-
-                            // Create the Chart object
-                            var line_example_chart = new Chart(ctx).Line(data);
-
-                            // Used for the labels on the X axis
-                            var label_idx = 1;
-
-                            // Function to execute to remove then add a new random value to the chart
-                            function rand_value() {
-                                // Generate a random integer
-                                var rand_val = Math.floor(Math.random() * 100);
-
-                                // Remove the point at the far left of the chart
-                                line_example_chart.removeData();
-
-                                // Add the random value at the far right of the chart
-                                line_example_chart.addData([rand_val], label_idx++);
+                        var dataCharts = [15, 19, 3, 5, 2, 3]
+                        var ctx = document.getElementById("myChart");
+                        var myChart = new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                                datasets: [{
+                                    label: '# of Votes',
+                                    data: dataCharts,
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255,99,132,1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)'
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:true
+                                        }
+                                    }]
+                                }
                             }
-                            // Run rand_value() every 2 seconds
-                            window.setInterval(rand_value, 2000);
-                        }
+                        });
                 </script>
                 </div>
             </div>

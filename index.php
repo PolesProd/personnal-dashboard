a868633b023f785e77ce7aebcd61346dde665cf4
Gootime <gotimeingame@gmail.com>
2016-09-19 10:55:10 +0200

    test

--- index.php
+++ index.php
@@ -135,34 +135,33 @@
          });
          });
       </script>
-      <div class="WidgetGrand" id='mySwipe'>
-          <div class='swipe-wrap'>
-            <div><b>0</b></div>
-            <div><b>1</b></div>
-            <div><b>2</b></div>
-            <div><b>3</b></div>
-            <div><b>4</b></div>
-            <div><b>5</b></div>
-            <div><b>6</b></div>
-            <div><b>7</b></div>
-            <div><b>8</b></div>
-            <div><b>9</b></div>
-            <div><b>10</b></div>
-            <div><b>11</b></div>
-            <div><b>12</b></div>
-            <div><b>13</b></div>
-            <div><b>14</b></div>
-            <div><b>15</b></div>
-            <div><b>16</b></div>
-            <div><b>17</b></div>
-            <div><b>18</b></div>
-            <div><b>19</b></div>
-            <div><b>20</b></div>
-          </div>
-          <button onclick='mySwipe.prev()'>Prev</button> 
-          <button onclick='mySwipe.next()'>Next</button>
-    </div>
-</div>
+      <div class="WidgetGrand">
+         <div  style='max-width:500px;margin:0 auto' class='swipe'>
+              <div class='swipe-wrap'>
+                <div><b>0</b></div>
+                <div><b>1</b></div>
+                <div><b>2</b></div>
+                <div><b>3</b></div>
+                <div><b>4</b></div>
+                <div><b>5</b></div>
+                <div><b>6</b></div>
+                <div><b>7</b></div>
+                <div><b>8</b></div>
+                <div><b>9</b></div>
+                <div><b>10</b></div>
+                <div><b>11</b></div>
+                <div><b>12</b></div>
+                <div><b>13</b></div>
+                <div><b>14</b></div>
+                <div><b>15</b></div>
+                <div><b>16</b></div>
+                <div><b>17</b></div>
+                <div><b>18</b></div>
+                <div><b>19</b></div>
+                <div><b>20</b></div>
+              </div>
+        </div>
+      </div>
       
    </div>
    <footer>

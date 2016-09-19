824cb4349f7aed8563d3a9e0c2eb6f2e7df38e6e
Gootime <gotimeingame@gmail.com>
2016-09-19 11:01:12 +0200

    push

--- index.php
+++ index.php
@@ -159,17 +159,16 @@
 // pure JS
 var elem = document.getElementById('mySwipe');
 window.mySwipe = Swipe(elem, {
-  // startSlide: 4,
-  //auto: 3000,
-  // continuous: true,
-  // disableScroll: true,
-  // stopPropagation: true,
-  // callback: function(index, element) {},
-  // transitionEnd: function(index, element) {}
+   startSlide: 4,
+  auto: 3000,
+   continuous: true,
+   disableScroll: true,
+   stopPropagation: true,
+   callback: function(index, element) {},
+   transitionEnd: function(index, element) {}
 });
 
-// with jQuery
-// window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
+
 
 </script>
       </div>

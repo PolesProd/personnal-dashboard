72b5f7b464f311537bff741b35ecb6bffaf10864
Gootime <gotimeingame@gmail.com>
2016-09-19 13:42:30 +0200

    php

--- includes/db.php
+++ includes/db.php
@@ -7,4 +7,13 @@ try {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
+
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($db->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $db->error;
+}
 ?>

32e085b30e914589c39257a7e1124105eea8eab5
Gootime <gotimeingame@gmail.com>
2016-09-19 09:37:15 +0200

    p

--- index.php
+++ index.php
@@ -109,14 +109,14 @@ include('lib/rssclass.php');
                             },
                             series: [{
                                 name: 'Tokyo',
-                                data: Math.floor(Math.random() * 31)
+                                data: [15.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
                             }]
                         }
                         $('#container').highcharts(data);
                         var count = data.series[0].data.length
                         for (i = 0; i < count; i++) { 
                             data.series[0].data[i]
-                            data.series[0].data[i] = 
+                            data.series[0].data[i] = Math.floor(Math.random() * 31)
                              console.log(data.series[0].data[i])
                         }
                         

b0ff8355faff26cbdb0c51dfa38573f52ad8080f
Gootime <gotimeingame@gmail.com>
2016-09-19 16:19:05 +0200

    b

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include('lib/rssclass.php');
     include('includes/db.php');
 
-    $sqlSelect = 'SELECT * FROM table 2';
+    $sqlSelect = 'SELECT * FROM table_2';
     $querySelect = $db->query($sqlSelect);
     $resultSelect = $querySelect->fetchAll();
     $countResult = count($resultSelect);

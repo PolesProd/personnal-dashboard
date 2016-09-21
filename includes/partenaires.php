8850bbb4ffafe1b054da634ce6e132cbcf45b4ff
adrien centonze <adrien centonze>
2016-09-21 20:04:23 +0200

    rendu intermediaire sur les partenaires

--- includes/partenaires.php
+++ includes/partenaires.php
@@ -1,17 +1,37 @@
-<div>
-	<ul>
-		<?php
-		for($i = 0; $i< $countResult;$i++){?>
-			<li id="liste<?= $i; ?>" class="adherent">
-				<ul>
-					<img src="<?php echo $resultSelect[$i]['lienThumb']; ?>" alt="<?=$resultSelect[$i]['structure'];?>">
-					<li id="nom"><p><?=$resultSelect[$i]['nom'];?> <?=$resultSelect[$i]['prenom'];?></p></li>
-					<li id="fonction"><p><?=$resultSelect[$i]['fonction'];?></p></li>
-					<li id="structure"><p><?=$resultSelect[$i]['structure'];?></p></li>
-				</ul>
-			</li>
-		<?php        
+<?php
+	//Creation de 2 variables --1 / colonne **
+	$colLeft = array();
+	$colRight = array();
+	
+	//Les fonctions principales **
+	function genRaw($l){
+		return "<li id='liste". $l ."' class='adherent'>
+					<ul>
+						<img src='" . $resultSelect[$l]['lienThumb'] . "' alt='" . $resultSelect[$l]['structure'] . "'>
+						<li id='nom'><p>" . $resultSelect[$l]['nom'] . " " . $resultSelect[$l]['prenom'] . "</p></li>
+						<li id='fonction'><p>" .$resultSelect[$l]['fonction'] . "</p></li>
+						<li id='structure'><p>" . $resultSelect[$l]['structure']."</p></li>
+					</ul>
+				</li>";
+	}
+	function dataWrite($arrayData){
+		for( $i=0; $i<count($arrayData); $i++){
+			echo $arrayData[$i];
 		}
-		?>
-	</ul>
-</div>
+	}
+
+	//Repartition du contenus entre chaques colonnes **
+	for($i = 0; $i< $countResult;$i++){
+		if( $i%2 == 0 ){
+			$colRight[] = genRaw($i);
+		}elseif( $i%2 == 1 ){
+			$colLeft[] = genRaw($i);
+		}
+	}
+	//Insertion dans le DOM **?>
+	<ul id="colLeft"><?php
+		dataWrite($colLeft);
+	?></ul>
+	<ul id="colRight"><?php
+		dataWrite($colRight);
+	?></ul>

9794e5f7b7c6b68c2ccf45572ca8fae218fd20b5
adrien centonze <adrien centonze>
2016-09-21 16:28:45 +0200

    partenaire

--- includes/partenaires.php
+++ includes/partenaires.php
@@ -1,20 +1,17 @@
 <div>
 	<ul>
 		<?php
-			for($i = 0; $i< $countResult;$i++){?>
+		for($i = 0; $i< $countResult;$i++){?>
 			<li id="liste<?= $i; ?>" class="adherent" style="background-image:url('<?php echo $resultSelect[$i]['lienThumb']; ?>');">
 				<ul>
 					<li id="nom"><p><?=$resultSelect[$i]['nom'];?></p></li>
 					<li id="prenom"><p><?=$resultSelect[$i]['prenom'];?></p></li>
 					<li id="fonction"><p><?=$resultSelect[$i]['fonction'];?></p></li>
 					<li id="structure"><p><?=$resultSelect[$i]['structure'];?></p></li>
-					<li id="postal"><p><?=$resultSelect[$i]['code_postal'];?></p></li>
-					<li id="ville"><p><?=$resultSelect[$i]['ville'];?></p></li>
-					<li id="email"><p><?=$resultSelect[$i]['email'];?></p></li>
 				</ul>
 			</li>
 		<?php        
-			}
+		}
 		?>
 	</ul>
 </div>

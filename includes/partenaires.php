041677cf8b048bd2a532c16c4fa341512a5f0c15
adrien centonze <adrien centonze>
2016-09-21 18:46:26 +0200

    plug in marche

--- includes/partenaires.php
+++ includes/partenaires.php
@@ -2,10 +2,10 @@
 	<ul>
 		<?php
 		for($i = 0; $i< $countResult;$i++){?>
-			<li id="liste<?= $i; ?>" class="adherent" style="background-image:url('<?php echo $resultSelect[$i]['lienThumb']; ?>');">
+			<li id="liste<?= $i; ?>" class="adherent">
 				<ul>
-					<li id="nom"><p><?=$resultSelect[$i]['nom'];?></p></li>
-					<li id="prenom"><p><?=$resultSelect[$i]['prenom'];?></p></li>
+					<img src="<?php echo $resultSelect[$i]['lienThumb']; ?>" alt="<?=$resultSelect[$i]['structure'];?>">
+					<li id="nom"><p><?=$resultSelect[$i]['nom'];?> <?=$resultSelect[$i]['prenom'];?></p></li>
 					<li id="fonction"><p><?=$resultSelect[$i]['fonction'];?></p></li>
 					<li id="structure"><p><?=$resultSelect[$i]['structure'];?></p></li>
 				</ul>
